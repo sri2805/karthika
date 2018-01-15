c945208ca2388b118254200c24dc89c2bb8b9183
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-15 11:24:32 +0530

    Update and rename LICENSE.md to k.c

---
+++ LICENSE.md/k.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n==0)
+{
+printf("the given number is zero");
+}
+if(n>0)
+{
+printf("the given number is positive"0;
+}
+if(n<0)
+{
+printf("the given number is negative");
+}
+return 0;
+}
