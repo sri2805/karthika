77c63e9b10a5a0c0ea50f57a4cea3855b77966cf
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-21 02:01:56 -0300

    Create armstrong number.c

---
+++ armstrong number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int num,result=0;rem;
+scanf("%d",&num);
+while(num!=0)
+{
+rem=num%10;
+result=result+rem*rem*rem
+num=num/10;
+}
+if(result==num)
+printf("armstrong");
+else
+printf("not armstrong");
+}
