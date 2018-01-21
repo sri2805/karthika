281560fd30ead3c65ba7caed89939cf0ddebf580
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-21 01:29:52 -0300

    Create count number.c

---
+++ count number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,a[i],i,k;
+printf("enter the number");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the limit");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+sum=sum+a[i];
+}
+printf("%d',sum);
+}
