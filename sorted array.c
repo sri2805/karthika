d44d055fd9990f7faf7f969137be0c4db3908fe0
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-24 16:51:51 +0530

    Create sorted array.c

---
+++ sorted array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i,j a[90],n,temp;
+printf("enter number");
+scanf("%d",&n);
+printf("enter the array");
+for(i=1;i<=n;i++)
+scanf("%d",&a[i]);
+for(i=1;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+for(i=1;i<=n;i++)
+printf("%d",a[i]);
+return 0;
+}
