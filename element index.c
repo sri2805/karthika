95b6e960dbdc7c31ba1cfc52c06b607e2e2998c8
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-31 16:08:11 +0530

    Create element index.c

---
+++ element index.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,b[10],a;
+printf("enter no of elements in an array");
+scanf("%d",&a);
+printf("enter the elements in an array");
+for(j=0;j<c;i++)
+{
+scanf("%d\n",&a[j]);
+}
+for(j=0;j<a;j++)
+{
+printf("a[%d]=%d\n",j,a[j]);
+}
+}
