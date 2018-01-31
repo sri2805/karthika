930a2290657a75ca9cbb5019966db92ae2c29252
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-31 16:43:14 +0530

    Create no of words.c

---
+++ no of words.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char b[200];
+int count=0,i;
+printf("Enter the string\n");
+scanf("%d[^\n]b",b);
+for(i=0;b[i]!='\0';i++)
+{
+if(b[i]== ' ')
+count++;
+}
+printf("Number of words in given string are: %d\n",count++1);
+}
