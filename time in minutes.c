93e6053e75cf2f2ab27c5219ab15e0c639515ea9
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-31 16:19:21 +0530

    Create time in minutes.c

---
+++ time in minutes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h,m,total mintues;
+printf("enter the times in hours");
+printf("hours");
+printf("mintues");
+scanf("%d,&m);
+total mintues=(h+60)+m;
+printf("enter the mintues");
+}
