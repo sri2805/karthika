5d8a1d5bcc69f25a370e07d4d8b4dff22cb34619
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-01-31 16:31:42 +0530

    Create same format.c

---
+++ same format.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h,m,b;
+printf("enter the times in hours and mintue");
+scanf("%d",& hours);
+printf("%d",&mintues);
+a=h-m;
+printf("enter the subract values");
+}
