60ad9da3dd3a2bc5e932efd50afcba45a5c4921d
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-02-05 16:03:10 +0530

    Update no.of special char.c

--- no.of special char.c
+++ no.of special char.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-int main()
-{
-	int count=0,i;
-	char b[50];
-	gets(b);
-	for(i=0;b[i];i++)
-	{	
-         if(ispunct(b[i]))
-         {
-         	count++;
-         }
 
-	}
-	printf("%d",count);
-	return 0;
-}
