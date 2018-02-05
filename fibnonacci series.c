219b5829512ef55962a31534f361cf51e658445d
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-02-05 16:42:56 +0530

    Create fibnonacci series.c

---
+++ fibnonacci series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n1,a=0,b=1,e;
+	printf("value");
+	scanf("%d",&n1);
+	for(i=1;i<n;i++)
+	{
+		printf("\n %d",a);
+		e=a+b;
+		a=b;
+		e=b;
+	}
+	printf("\n %d",e);
+	return 0;
+  }
