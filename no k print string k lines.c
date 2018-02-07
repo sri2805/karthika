cbb1708734d714e0a1b1d0f3d1f5172c5a77d112
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-02-07 15:39:58 +0530

    Create no k print string k lines.c

---
+++ no k print string k lines.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+char b[50];
+printf("enter the value of n and b");
+scanf("%s%d",b,n);
+printf("\n %s%d",b,n);
+for(i=1;i<=n;i++);
+{
+printf("%s",b);
+}
+return 0;
+}
