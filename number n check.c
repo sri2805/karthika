f0c6a89fc8cf32fa7cd20cf74fbdd8e179d89c50
sri2805 <35136047+sri2805@users.noreply.github.com>
2018-02-09 15:49:12 +0530

    Create number n check.c

---
+++ number n check.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    char b[5]={'y','e','s'};
+    char c[5]={'n','o'};
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("%s",yes);
+    }
+    else
+    {
+        printf("%s",no);
+    }
+    }
